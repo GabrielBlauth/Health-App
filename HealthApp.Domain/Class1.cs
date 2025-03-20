f39c3f4ccb4ab70fdb92ab943b3f18f684f8d376
Gabriel Blauth <149583844+GabrielBlauth@users.noreply.github.com>
2025-03-20 14:24:36 +0000

    Add files via upload

--- HealthApp.Domain/Class1.cs
+++ HealthApp.Domain/Class1.cs
@@ -1,7 +1,26 @@
 ﻿namespace HealthApp.Domain
 {
-    public class Class1
+    public class Admin
     {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
 
+    public class Doctor
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class Patient
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
